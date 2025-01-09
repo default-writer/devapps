ba5515e68770e87cda0f2483ac701019ed4e2d63
default-writer <funcelot@outlook.com>
2025-01-09 17:19:36 +0000

    added firezone server script

---
+++ firezone.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+if [[ "${BASHOPTS}" != *extdebug* ]]; then
+    set -e
+fi
+
+# define functions
+err_report() {
+    echo "ERROR: $0:$*"
+    exit 8
+}
+
+function help() {
+    local source=$0
+    local script="$(basename "$(test -L "${source}" && readlink "${source}" || echo "${source}")")"
+    local commands=$(cat ${source} | sed -e 's/^[ \t]*//;' | sed -e '/^[ \t]*$/d' | sed -n -e 's/^"\(.*\)".*#/    \1:/p' | sed -n -e 's/: /:\n        /p')
+    echo "$(cat ${source} | sed -e 's/^[ \t]*//;' | sed -e '/^[ \t]*$/d' | sed -n -e 's/^## \(.*\).*/\1/p' | script=""${script}"" commands=""${commands}"" envsubst)"
+    exit 8
+}
+
+function random() {
+    local set="abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    local rand=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w "$1" | head -n 1)
+    echo $rand
+}
+
+# set error handler
+if [[ "${BASHOPTS}" != *extdebug* ]]; then
+    trap 'err_report $LINENO' ERR
+fi
+
+# get path
+pwd="$(pwd)"
+
+## Create directories and files
+## Usage: ${script} <option>
+## ${commands}
+
+while (($#)); do
+    case "$1" in
+        "--install") # installs firezone
+            bash <(curl -fsSL https://github.com/firezone/firezone/raw/legacy/scripts/install.sh)
+            ;;
+        "--help") # [optional] shows command description
+            help
+            ;;
+        *)
+            help
+            ;;
+    esac
+    shift
+done
+
+[[ $SHLVL -gt 2 ]] || echo OK
+
+cd "${pwd}"
+
